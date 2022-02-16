0dc609a972f18d4e50cfd36544104bdcda4b7ff7
Andrew Kingery <kandrew2022@gmail.com>
2022-02-16 18:23:26 -0500

    Add Intake

--- src/main/java/frc/team3324/robot/RobotContainer.java
+++ src/main/java/frc/team3324/robot/RobotContainer.java
@@ -2,13 +2,20 @@ package frc.team3324.robot;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.team3324.library.commands.MotorCommand;
+import frc.team3324.library.motorcontrollers.SmartMotorController;
+import frc.team3324.library.subsystems.MotorSubsystem;
 import frc.team3324.robot.drivetrain.DriveTrain;
 import frc.team3324.robot.drivetrain.commands.Drive;
+import frc.team3324.robot.util.Consts;
 import io.github.oblarg.oblog.Logger;
 
 class RobotContainer {
     public DriveTrain driveTrain = new DriveTrain();
+
+    public MotorSubsystem intake = new MotorSubsystem(new SmartMotorController[]{Consts.Intake.MOTOR}, 0.0);
 
     private NetworkTableInstance table = NetworkTableInstance.getDefault();
 
@@ -39,6 +46,9 @@ class RobotContainer {
     private void configureButtonBindings() {
         // Start accepting input from controller for drivetrain
         driveTrain.setDefaultCommand(new Drive(driveTrain, primaryController::getRightX, primaryController::getLeftY));
+
+        new JoystickButton(primaryController, Button.kX.value).whileHeld(new MotorCommand(intake, 1.0, true, () -> {return false;}));
+        new JoystickButton(primaryController, Button.kY.value).whileHeld(new MotorCommand(intake, -1.0, true, () -> {return false;}));
     }
 
     private void rumbleController(double rumbleLevel) {
